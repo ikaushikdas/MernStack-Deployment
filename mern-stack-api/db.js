49e0d73a57f70505b7fa899aea9594535959106c
Kaushik Das <kaushik.das@codeclouds.in>
2023-06-08 18:54:04 +0530

    Updated files

--- mern-stack-api/db.js
+++ mern-stack-api/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/postManagerDB',{useNewUrlParser:true,useUnifiedTopology:true},
+mongoose.connect('mongodb://mongo:27017/postManagerDB',{useNewUrlParser:true,useUnifiedTopology:true},
     err => {
         if (!err)
             console.log('Mongodb connection succeeded.')
--- mern-stack-api/index.js
+++ mern-stack-api/index.js
@@ -8,7 +8,7 @@ var postMessageRoutes = require('./controllers/postMessageController')
 
 var app = express()
 app.use(bodyParser.json())
-app.use(cors({origin:'http://localhost:3000'}))
+app.use(cors({origin:'http://frontend:3001'}))
 app.listen(4000,()=>console.log('Server started at : 4000'))
 
 
