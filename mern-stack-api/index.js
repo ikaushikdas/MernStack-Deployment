830a411d9cc8c7a857d4ee9e06febbc5452ba45f
Kaushik Das <kaushik.das@codeclouds.in>
2023-06-08 20:42:31 +0530

    Again Updated

--- mern-stack-api/index.js
+++ mern-stack-api/index.js
@@ -7,8 +7,15 @@ var postMessageRoutes = require('./controllers/postMessageController')
 
 
 var app = express()
+
 app.use(bodyParser.json())
 app.use(cors({origin:'http://frontend:3001'}))
+app.options('/postmessages', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+  });
 app.listen(4000,()=>console.log('Server started at : 4000'))
 
 
